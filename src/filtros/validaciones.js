a3c632856533ddb11362acbc983f59f6a782e7c7
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-08-26 18:53:17 -0300

    finalizamos el ejercicio

--- src/filtros/validaciones.js
+++ src/filtros/validaciones.js
@@ -1,1 +1,20 @@
+export const validaciones = [
+  {
+    nombre: "stock",
+    fn: (valor, producto) => {
+      return producto.stock >= valor;
+    },
+  },
+  {
+    nombre: "categoria",
+    fn: (valor, producto) => {
+      return valor.includes(producto.categorias);
+    },
+  },
+];
 
+export function ejecutarValidacion(nombre, valor, producto) {
+  const validacion = validaciones.find((v) => v.nombre === nombre);
+  if (!validacion) return false;
+  return validacion.fn(valor, producto);
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-const express = require("express");
+
+import express from "express";
+import { ejecutarValidacion } from "./filtros/validaciones.js";
+import { readFileSync } from "fs";
+
+const productos = JSON.parse(
+  readFileSync(new URL("../data/productos.json", import.meta.url))
+);
+
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.post("/productos/filtrar", (req, res) => {
+  const filtros = req.body; // 
+
+  const resultado = productos.filter((producto) =>
+    filtros.every((f) => ejecutarValidacion(f.fn, f.values, producto))
+  );
+
+  res.json(resultado);
 });
 
-const mensaje = "escuchando en el puerto http://localhost:3000";
-app.listen(3000, () => {
-  console.log(mensaje);
-});
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`)
+);
