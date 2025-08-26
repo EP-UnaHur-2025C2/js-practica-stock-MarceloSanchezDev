b3d10179970af1175f90227de2abc7dac200aa4f
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-08-26 17:51:17 -0300

    inicialice express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+const mensaje = "escuchando en el puerto http://localhost:3000";
+app.listen(3000, () => {
+  console.log(mensaje);
+});
